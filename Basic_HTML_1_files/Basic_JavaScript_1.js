2a27823848896ee85bd5f70094e34f674b33b1be
Nathan Skiles <nateskiles94@gmail.com>
2021-02-09 12:49:34 -0800

    Turn in assignment
    For assignment on pg. 134

---
+++ Basic_HTML_1_files/Basic_JavaScript_1.js
@@ -0,0 +1,11 @@
+function my_alert() {
+    alert("Hello world! I am an alert box!");
+}
+
+function My_FirstFunction() {
+    var String = "Kiss me, I'm Irish!";
+    var result = String.fontcolor("green");
+    document.getElementById("Irish").innerHTML = 
+    result;
+}
+    
